e709390db8b109f034aaf1cb74b0a94dac26e113
Jitu <Jitender@gamil.com>
2020-01-14 18:38:23 +0530

    First commit to project 2020

---
+++ controller/employeectrl.js
@@ -0,0 +1,6 @@
+app.controller("myCtrl", function($scope) { 
+var employees = ['Catherine Grant', 'Monica Grant',
+'Christopher Grant', 'Jennifer Grant'
+];
+$scope.ourEmployees = employees;
+});
---
+++ js/myApp.js
@@ -0,0 +1,8 @@
+var app = angular.module("myApp", []);
+app.controller('myCtrl', function($scope) { 
+    var employees = ['Catherine Grant', 'Monica Grant',
+    'Christopher Grant', 'Jennifer Grant'
+    ];
+    $scope.ourEmployees = employees;
+    }); 
+    
